e59109892db94ff892d8a69544574c462597b77f
Hacer Yavas <haceryavaas@gmail.com>
2025-07-16 11:48:11 +0300

    vki_hesaplama

---
+++ src/Week1/VKIHesaplama.java
@@ -0,0 +1,33 @@
+package Week1;
+
+import java.util.Scanner;
+
+/**
+ * @author Hacer Nur YAVAŞ - 26 Mayıs 2025
+ * Bu proje Vücut Kitle İndeksi hesaplar.
+ */
+public class VKIHesaplama {
+    public static void main(String[] args) {
+
+        int weight;
+        double height, VKI;
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Lütfen boyunuzu metre cinsinden giriniz: ");
+        height = inp.nextDouble();
+        if (height <= 0) {
+            System.out.println("Boyunuz pozitif bir sayı olmalı.");
+            return;
+        }
+
+        System.out.print("Lütfen kilonuzu giriniz: ");
+        weight = inp.nextInt();
+        if (weight <= 0) {
+            System.out.println("Kilonuz pozitif bir sayı olmalı.");
+            return;
+        }
+
+        VKI = weight / (height * height);
+        System.out.println("Vücut Kitle İndeksi: " + VKI);
+    }
+}
