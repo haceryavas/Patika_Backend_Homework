36ccc3c5009f2e8843710629a0fffa6a3e5148ef
Hacer Yavas <haceryavaas@gmail.com>
2025-08-12 14:56:33 +0300

    inner2

--- src/week5/innerClasses/EmployeeTest.java
+++ src/week5/innerClasses/EmployeeTest.java
@@ -2,6 +2,28 @@ package week5.innerClasses;
 
 import java.util.Scanner;
 
+/**
+ * This program demonstrates the use of an Inner Class in Java to store
+ * and manage employee contact information.
+ *
+ * The outer class `Employee` stores personal details such as the employee's
+ * name and surname, while the inner class `ContactInfo` stores contact
+ * details such as the mobile phone number and email address.
+ *
+ * In the `EmployeeTest` class, the program takes user input for both personal
+ * and contact information, creates an `Employee` object, and then prints all
+ * details using the `print` method.
+ *
+ * Key features:
+ * - Demonstrates encapsulation by using an inner class to group related data.
+ * - Shows how to initialize and use an inner class instance inside the outer class.
+ * - Uses the `Scanner` class to collect data from the user.
+ *
+ * @author Hacer Nur YAVAŞ - 12 August 2025
+ */
+
+
+
 public class EmployeeTest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
